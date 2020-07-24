2641c95c336f61ae39278854fd4a85c99538550a
bigfoxtail <bigfoxtail@outlook.com>
2020-07-24 14:21:12 +0800

    调整配置

--- init.sh
+++ init.sh
@@ -2,16 +2,11 @@
 if [ ! -d "./server/caddy_data" ];then
   mkdir -p ./server/caddy_data
 fi
-if [ ! -f "./server/config.json" ];then
-  cp v2_config.json ./server/v2_config.json
-fi
-if [ ! -f "./server/Caddyfile" ];then
-  cp Caddyfile ./server/Caddyfile
-fi
+cp v2_config.json ./server/v2_config.json
+cp Caddyfile ./server/Caddyfile
 
 CADDY_DOMAIN=www.test.com
 V2RAY_UUID=69f9d2f3-89af-4d39-b80c-d4c0c7116086
 
 sed -i "s/CADDY_DOMAIN/$CADDY_DOMAIN/" ./server/Caddyfile
 sed -i "s/V2RAY_UUID/$V2RAY_UUID/" ./server/v2_config.json
-
