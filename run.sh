65b3ccaee18fe3dd431ff9ae60741a8bf3a99cb5
bigfoxtail <bigfoxtail@outlook.com>
2020-07-24 11:54:59 +0800

    20200724

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-if [ ! -d "/docker_data/" ];then
-  mkdir /docker_data
+if [ ! -d "./server/caddy_data" ];then
+  mkdir -p ./server/caddy_data
+fi
+if [ ! -f "./server/config.json" ];then
+  cp v2_config.json ./server/v2_config.json
+fi
+if [ ! -f "./server/Caddyfile" ];then
+  cp Caddyfile ./server/Caddyfile
 fi
 
-sed -i "s/CADDY_DOMAIN/$CADDY_DOMAIN/" /etc/caddy/Caddyfile
-sed -i "s/V2RAY_UUID/$V2RAY_UUID/" /etc/v2ray/config.json
+CADDY_DOMAIN=www.test.com
+V2RAY_UUID=69f9d2f3-89af-4d39-b80c-d4c0c7116086
 
-caddy start --config /etc/caddy/Caddyfile --adapter caddyfile
-v2ray -config=/etc/v2ray/config.json
+sed -i "s/CADDY_DOMAIN/$CADDY_DOMAIN/" ./server/Caddyfile
+sed -i "s/V2RAY_UUID/$V2RAY_UUID/" ./server/v2_config.json
+
