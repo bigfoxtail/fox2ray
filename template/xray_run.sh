52b29b01da6d1ddf6686b42e3392837cc7c576f4
bigfox <bigfoxtail@outlook.com>
2021-06-19 18:33:14 +0800

    更新配置

--- template/xray_run.sh
+++ template/xray_run.sh
@@ -7,8 +7,6 @@ do
         if [ -n "$domain" ]; then
             crtpath=$domain
             keypath=${crtpath/.crt/.key}
-            echo "1${crtpath}"
-            echo "2${keypath}"
             rm /etc/xray/startconfig.json
             cp /etc/xray/config.json /etc/xray/startconfig.json
             sed -i "s#/path/to/certificate.crt#$crtpath#" /etc/xray/startconfig.json
